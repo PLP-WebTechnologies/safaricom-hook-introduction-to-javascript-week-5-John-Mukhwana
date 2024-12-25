20abb783462f7b4e53976b3ea92a1d3fa7037f83
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 11:35:51 +0300

    Add JavaScript basics section with variables and data types

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// part 1: Javascript Basics
+
+// 1. Varibles and Data Types
+
+const name='John';
+const age=30;
+const isStudent=false;
+const hobbies=['music','movies','sports'];
+const person={name:'Jane',age:20,isStudent:true, course:'Nursing'};
+
+console.log(`Name: ${name} (Type: ${typeof name})`);
