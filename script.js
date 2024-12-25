1299f3b66990dbc39b724a72be1b98f6612eaaf5
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 12:12:27 +0300

    Rename number list to name list and implement calculator, greeting, voting eligibility, and dynamic content generation features

--- script.js
+++ script.js
@@ -16,6 +16,73 @@ console.log(`Hobbies: ${hobbies} (Type: ${typeof hobbies})`);
 console.log(`Person: ${person} (Type: ${typeof person})`);
 
 // 2. Operators
- const calculator =()=>{
-    
+ function calculator(){
+    const num1=parseFloat(prompt('Enter first number: '));
+    const num2=parseFloat(prompt('Enter second number: '));
+    const operation=prompt("chose operation: +, -, *, /");
+
+    if(isNaN(num1) || isNaN(num2)){
+        alert("Invalid Input.Please Enter valid numbers.");
+        return;
+    }
+
+    let result;
+    switch(operation){
+        case '+':
+            result=num1+num2;
+            break;
+        case '-':
+            result=num1-num2;
+            break;
+        case '*':
+            result=num1*num2;
+            break;
+        case '/':
+            result = num2 !== 0 ? num1 / num2 : "Division by zero is not allowed";
+            break;
+        default:
+            alert("Invalid operation");
+            return;
+    }
+    alert(`Result: ${result}`);
  }
+    calculator();
+
+// 3.functions
+
+function greet(name){
+    return `Hello ${name} ! Welcome to Javascript`;
+}
+document.getElementById("greeting").textContent=greet('John');
+
+/*Part 2: Javascript Control Structures*/
+// 1. If Statement
+
+const votingEligibility=()=>{
+    const userAge=parseInt(prompt("Enter your age: "));
+    const message= userAge >= 18? "You are eligible to vote" : "You are not eligible to vote";
+    document.getElementById("vote-message").textContent=message;
+}
+votingEligibility();
+
+//2. loops
+
+const nameList=document.getElementById("name-list");
+const names=['John','Jane','Mary','Mark','James'];
+for(let i=0; i<names.length; i++){
+    const listItem=document.createElement('li');
+    listItem.textContent=names[i];
+    nameList.appendChild(listItem);
+}
+
+//Part 3: Introduction to DOM Manipulation
+
+// Changing the text <h1> element
+
+document.querySelector("h1").textContent="Javascript Basics";
+
+const dynamicTextDiv=document.getElementById("dynamic-content");
+const newParagraph=document.createElement('p');
+newParagraph.textContent="This conten is generated dynamically using Javascript";
+dynamicTextDiv.appendChild(newParagraph);
+
